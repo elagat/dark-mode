10515a2621273c3bc52233b9919abe27434f6442
Emily Lagattolla <elagattolla@gmail.com>
2019-07-25 17:58:59 -0500

    WIP: returning setter function array to toggle dark mode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage.js"
 
 const useDarkMode = () {
-  const [On, setOn] = useLocalStorage('darkModeOn')
+  const [on, setOn] = useLocalStorage('darkModeOn')
   useEffect(() => {
     if (useLocalStorage() === 'darkModeOn') {
-      return document.getElementByTagName("body").className = "dark-mode";
+      document.getElementByTagName("body").classList.add = "dark-mode";
+      return T;
     } else {
-      return document.getElementByTagName("body").classList.remove = "dark-mode"; 
+      document.getElementByTagName("body").classList.remove = "dark-mode";
+      return F;
     }
-  })
-}
+  }, []);
 
-myStyle.insertRule('#blanc { color: white }', 0);
+  return [useLocalStorage({name}), useLocalStorage({name.value})
+}
